033c451dff736d00b240cf625d161e74d8d8019b
Joy Sarkar <joysarkar8171@gmail.com>
2024-04-19 13:01:04 +0530

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class SendMessageWidget extends StatefulWidget {
@@ -31,20 +32,28 @@ class _SendMessageWidgetState extends State<SendMessageWidget> {
       String phoneNumber = _phoneNumberController.text;
 
       // Clear text fields after sending
-      sendSms();
+      sendSms(msg: message, number: phoneNumber);
       print("sms sent");
+      Fluttertoast.showToast(
+          msg: "SMS Sent",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
       _messageController.clear();
       _phoneNumberController.clear();
     }
   }
 
-  Future<Null> sendSms() async {
+  Future<Null> sendSms({required String msg, required String number}) async {
     print("SendSMS");
     try {
       final String result = await platform.invokeMethod(
           'send', <String, dynamic>{
-        "phone": "+917001577583",
-        "msg": "Hello! I'm sent programatically."
+        "phone": "+91$number",
+        "msg": msg
       }); //Replace a 'X' with 10 digit phone number
       print(result);
     } on PlatformException catch (e) {
@@ -56,7 +65,10 @@ class _SendMessageWidgetState extends State<SendMessageWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Send Message'),
+        title: const Text(
+          'Send SOS',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -65,20 +77,20 @@ class _SendMessageWidgetState extends State<SendMessageWidget> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              TextFormField(
-                controller: _messageController,
-                decoration: InputDecoration(labelText: 'Message'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a message';
-                  }
-                  return null;
-                },
+              const SizedBox(
+                height: 20,
               ),
-              SizedBox(height: 16.0),
               TextFormField(
                 controller: _phoneNumberController,
-                decoration: InputDecoration(labelText: 'Phone Number'),
+                decoration: const InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+                    labelText: 'Phone Number',
+                    prefixIcon: Icon(
+                      Icons.phone,
+                      color: Colors.black87,
+                    ),
+                    border: OutlineInputBorder(borderSide: BorderSide())),
                 keyboardType: TextInputType.phone,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 validator: (value) {
@@ -91,10 +103,59 @@ class _SendMessageWidgetState extends State<SendMessageWidget> {
                   return null;
                 },
               ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _sendMessage,
-                child: Text('Send'),
+              SizedBox(height: 30.0),
+              TextFormField(
+                controller: _messageController,
+                decoration: const InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+                    prefixIcon: Icon(
+                      Icons.message,
+                      color: Colors.black87,
+                    ),
+                    labelText: 'Message',
+                    border: OutlineInputBorder(borderSide: BorderSide())),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a message';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 30.0),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 163, 8, 112),
+                      Color.fromARGB(255, 54, 8, 92)
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: ElevatedButton(
+                  onPressed: _sendMessage,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    child: const Text(
+                      'Send',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
